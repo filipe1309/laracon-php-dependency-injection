ffecd4e5fffc6d9fcc4212de0148bb7ebb024f86
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-25 00:52:36 -0300

    feat: add Container class

---
+++ src/Config/Container.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laracon\DependencyInjection\Config;
+
+class Container
+{
+  private $bindings = [];
+
+  public function set($abstract, callable $factory)
+  {
+    $this->bindings[$abstract] = $factory;
+  }
+
+  public function get($abstract)
+  {
+    return $this->bindings[$abstract]($this);
+  }
+}
