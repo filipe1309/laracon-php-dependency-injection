2c571c04cecee18e989703b45107ee57a0076f72
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-25 01:01:34 -0300

    refactor: replace direct instation with Container injection

--- test.php
+++ test.php
@@ -3,12 +3,25 @@
 require 'vendor/autoload.php';
 
 use Laracon\DependencyInjection\Application\Helper\Logger;
+use Laracon\DependencyInjection\Config\Container;
 use Laracon\DependencyInjection\Domain\User;
 use Laracon\DependencyInjection\Infrastructure\SessionStorage;
 
-$logger = new Logger();
-$storage = new SessionStorage($logger);
-$user = new User($storage);
+$container = new Container();
+
+$container->set(User::class, function (Container $c) {
+  return new User($c->get(SessionStorage::class));
+});
+
+$container->set(SessionStorage::class, function (Container $c) {
+  return new SessionStorage($c->get(Logger::class));
+});
+
+$container->set(Logger::class, function (Container $c) {
+  return new Logger();
+});
+
+$user = $container->get(User::class);
 $user->setLanguage('pt-BR');
 echo PHP_EOL . 'Language: ' .  $user->getLanguage() . PHP_EOL;
 // Language: pt-BR
