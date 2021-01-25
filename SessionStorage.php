1ca9d19a3b560707e2d15887479d5ce7b509b95a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-24 23:42:59 -0300

    feat: add SessionStore class

---
+++ SessionStorage.php
@@ -0,0 +1,19 @@
+<php
+
+class SessionStorage 
+{
+  public function __construct()
+  {
+    session_start();
+  }
+
+  public function set($key, $value)
+  {
+    $_SESSION[$key] = $value;
+  }
+
+  public function get($key)
+  {
+    return $_SESSION[$key];
+  }
+}
