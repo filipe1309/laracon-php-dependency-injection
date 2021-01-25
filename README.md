eb7964abfa5607730c8118ac4f01f297dbee46be
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-25 00:20:12 -0300

    refacto: improve test file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# Demystifying Dependency Injection Containers by Kai Sassnowski
+
+https://www.youtube.com/watch?v=y7EbrV4ChJs&ab_channel=LaraconEU
+
+
+```
 composer init
+composer install
+
+php test.php
+```
--- test.php
+++ test.php
@@ -5,9 +5,12 @@ require 'vendor/autoload.php';
 use Laracon\DependencyInjection\Domain\User;
 use Laracon\DependencyInjection\Infrastructure\SessionStorage;
 
-$user = new User(new SessionStorage());
+$storage = new SessionStorage();
+$user = new User($storage);
 $user->setLanguage('pt-BR');
 echo PHP_EOL . 'Language: ' .  $user->getLanguage() . PHP_EOL;
+// Language: pt-BR
+
 echo PHP_EOL;
 
 // var_dump($_SESSION, $_SESSION['language']);
